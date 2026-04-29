5f05588c8532fe1c01c2b7596fee6bc8ab47772e
kfyums <kfyums@gmail.com>
2026-04-29 08:30:11 +0300

    Update Equation.cpp

--- Equation.cpp
+++ Equation.cpp
@@ -10,11 +10,16 @@ Equation::Equation(double a, double b, double c)
 
 void Equation::solve(double& x1, double& x2) const 
 {
-    if (a == 0) 
-    {
-        x1 = x2 = 0;
-        return;
-    }
+     if (a == 0)
+ {
+     if (b == 0)
+     {
+         x1 = x2 = 0;
+     }
+     x1 = -c / b;
+     x2 = x1;  
+     return;
+ }
     double D = b * b - 4 * a * c;
     if (D < 0) 
     {
