eb81b0adc5348cf5a1ee286e6ca4f91a5f930a94
kfyums <kfyums@gmail.com>
2026-04-29 08:20:41 +0300

    Create Equation.cpp

---
+++ Equation.cpp
@@ -0,0 +1,26 @@
+#include "Equation.h"
+#include <cmath>
+
+Equation::Equation(double a, double b, double c) 
+{
+    this->a = a;
+    this->b = b;
+    this->c = c;
+}
+
+void Equation::solve(double& x1, double& x2) const 
+{
+    if (a == 0) 
+    {
+        x1 = x2 = 0;
+        return;
+    }
+    double D = b * b - 4 * a * c;
+    if (D < 0) 
+    {
+        x1 = x2 = 0;
+        return;
+    }
+    x1 = (-b + std::sqrt(D)) / (2 * a);
+    x2 = (-b - std::sqrt(D)) / (2 * a);
+}
