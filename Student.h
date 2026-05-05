f009c5f24f4c384df66d618d72b3fc876febeca0
kfyums <kfyums@gmail.com>
2026-05-05 18:43:19 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <string>
-
 enum class StudentType 
 {
     good,
     medium,
     bad
 };
-
 class Student 
 {
-public:
+private:
     std::string name;
     StudentType type;
-    Student(std::string name, StudentType type);
+public:
+    Student(const std::string& name, StudentType type);
+    const std::string& getName() const;
     void solve(double a, double b, double c, double& x1, double& x2) const;
 };
